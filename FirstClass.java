3de00f7d03fb241a6991014e1d91d3d5e7db55ce
soumya-b1 <105643403+soumya-b1@users.noreply.github.com>
2022-06-01 11:57:22 +0530

    Create FirstClass.java

---
+++ FirstClass.java
@@ -0,0 +1,6 @@
+public class FirstClass{
+public static void main (String[] args){
+System.out.println("Hello, Welcome");
+System.out.println("This is the code implementation for migration of data warehouse for Globe-Tech incorporation");
+}
+}
